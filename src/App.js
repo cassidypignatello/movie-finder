1ea5abca041c0be1b19f405b102d2d8ee5040e9f
cassidypignatello <cassidy.pignatello@gmail.com>
2018-03-11 21:43:06 -0400

    creates Header component and general app structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <div>
-      <h1>Movie Finder</h1>
+      <Header />
     </div>
   )
 }
